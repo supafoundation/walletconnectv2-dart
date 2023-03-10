fb3fa76872762d2f906b2d8c026f1e4d50b9c11a
apple <apple@192.168.102.48>
2023-03-11 00:54:48 +0700

    update lib w2

--- lib/supa_wc_v2.dart
+++ lib/supa_wc_v2.dart
@@ -182,9 +182,15 @@ class SupaWcV2 {
   }
 
   Future<void> removeSession() async{
-    await signClient.disconnectSession(topic: supaSessionData!.sessionData.topic, reason: WalletConnectError(code: 6000, message: "User disconnected."));
-    await storage.delete(key: sessionKeyStore);
-    initialized = false;
-    isFirstTimeConnect = false;
+    try {
+      if (initialized) {
+        await signClient.disconnectSession(topic: supaSessionData!.sessionData.topic, reason: WalletConnectError(code: 6000, message: "User disconnected."));
+        await storage.delete(key: sessionKeyStore);
+        initialized = false;
+        isFirstTimeConnect = false;
+      }
+    } catch(e){
+      print("Remove session error $e");
+    }
   }
 }
